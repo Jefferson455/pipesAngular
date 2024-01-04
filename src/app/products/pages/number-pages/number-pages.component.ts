7455a0f03fdbe42efcc87642addf410cca537cad
Jefferson455 <jeffersonguzman167@gmail.com>
2024-01-04 14:29:50 -0500

    Se agrega pipe currency y percentage en number-page

--- src/app/products/pages/number-pages/number-pages.component.ts
+++ src/app/products/pages/number-pages/number-pages.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './number-pages.component.css'
 })
 export class NumberPagesComponent {
-
+  public totalSeller: number = 24234.234123;
+  public percent: number = 1.343;
 }
